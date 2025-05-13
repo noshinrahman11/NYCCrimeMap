8574e1d1661cba57ffcf6ec759e754456f48af65
noshinrahman11 <noshinrahman11@gmail.com>
2025-05-13 03:43:30 -0400

    removed comments

--- app.py
+++ app.py
@@ -17,18 +17,6 @@ def map():
     # Create a Folium map centered on NYC
     nyc_map = folium.Map(location=[40.7128, -74.0060], zoom_start=10)
 
-    # # Add circles for each area
-    # for _, row in arrest_counts.iterrows():
-    #     folium.CircleMarker(
-    #         location=[row['rounded_lat'], row['rounded_lon']],
-    #         radius=row['count'] ** 0.5,  # scale radius
-    #         popup=f"Arrests: {row['count']}",
-    #         color='crimson',
-    #         fill=True,
-    #         fill_color='crimson',
-    #         fill_opacity=0.6
-    #     ).add_to(nyc_map)
-
     # Min arrests = 1, max = 490
     # print("min:", arrest_counts.min()) 
     # print("max:", arrest_counts.max())
@@ -37,7 +25,7 @@ def map():
     min_count = arrest_counts['count'].min()
     max_count = arrest_counts['count'].max()
     arrest_counts['normalized_count'] = (arrest_counts['count'] - min_count) / (max_count - min_count)
-    # Create a color map
+
     # colormap = folium.LinearColormap(['green', 'red'], vmin=0, vmax=1)
     colormap = folium.LinearColormap(['#fdaf9f', '#ff2d00'], vmin=0, vmax=1)
 
@@ -46,10 +34,9 @@ def map():
         folium.CircleMarker(
             location=[row['rounded_lat'], row['rounded_lon']],
             radius = 6,  # fixed size
-            color = colormap(row['normalized_count']),  # meaningful
+            color = colormap(row['normalized_count']),
             # radius=row['normalized_count'] * 20,  # scale radius
             popup=f"Arrests: {row['count']}",
-            # color=colormap(row['normalized_count']),
             fill=True,
             fill_color=colormap(row['normalized_count']),
             fill_opacity=0.6
